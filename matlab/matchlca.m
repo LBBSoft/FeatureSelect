90fd1d12a33b406f2f3e4796f67f25d2cb35451b
LBBSoft <42937478+LBBSoft@users.noreply.github.com>
2019-02-12 21:24:18 +0330

    Delete matchlca.m

--- matlab/matchlca.m
+++
@@ -1,9 +0,0 @@
-function [ output ] = matchlca( teams,mtime,input,label,tp,sv )
-  [m,n]=size(teams);
-  for i=1:m
-      for j=i+1:m
-          teams(i,:)=matchlcamain(teams(i,:),teams(j,:),mtime,input,label,tp,sv);
-      end
-  end
-  output=teams;
-end
